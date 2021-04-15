f7c0a9360c34ddf98fda2e54a18f68320f00fd3a
JacobEckroth <jacobeckroth@gmail.com>
2021-04-15 16:49:24 -0700

    list

--- calc.py
+++ calc.py
@@ -6,11 +6,13 @@ def calc(a,b):
     print(difference)
     multiply = a * b
     print(multiply)
+    divide = None
     if(b != 0):
         divide = a / b
         print(divide)
     else:
         print("You can't divide by zero!!!")
+    results = [sum,difference,multiply,divide]
 
 
 calc(4,3)
