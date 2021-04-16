1b1543a8fe0fb4c81b3841114d42e946ae99554d
JacobEckroth <jacobeckroth@gmail.com>
2021-04-15 17:04:24 -0700

    getting user input

--- divisor.py
+++ divisor.py
@@ -5,3 +5,10 @@ def divisors(num):
         if(num%i == 0):
             divisorsList.append(i)
     print(divisorsList)
+
+def getUserInput():
+    input = input("Please enter a positive integer:")
+
+
+
+getUserInput()
